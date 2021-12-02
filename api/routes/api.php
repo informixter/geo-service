77147036471381f45d930ead63dff860600a9a21
informixter <pbd.incom@gmail.com>
2021-12-02 23:36:38 +0500

    Fast commit at : 2021-12-02 23:36

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\RoutesController;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/routes', [RoutesController::class, 'index']);
 Route::post('/routes', [RoutesController::class, 'create']);
 Route::post('/routes/{id}', [RoutesController::class, 'update']);
+
+Route::post('/snap', function (Request $request) {
+    $end_point = env('SNAP_SERVER', "http://159.69.178.233:3000");
+    $client = new Client();
+    $response = $client->post($end_point,
+        [
+            \GuzzleHttp\RequestOptions::JSON => [
+                'token' => 'cd8c06d0040dac',
+                'data' => $request->get('data'),
+                'profile' => $request->get('profile', 'foot'),
+            ]
+        ]
+    );
+    $res = json_decode($response->getBody()->getContents(), true);
+    return response()->json($res);
+});
+
