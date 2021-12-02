3d94a0947bd0087303fe52f23e0dc001f0ea5e71
informixter <pbd.incom@gmail.com>
2021-12-03 00:17:19 +0500

    Fast commit at : 2021-12-03 00:17

--- api/app/Models/Routes.php
+++ api/app/Models/Routes.php
@@ -5,6 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Routes
+ *
+ * @property int $id
+ * @property string|null $name
+ * @property mixed $data
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Routes newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Routes newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Routes query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Routes whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Routes whereData($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Routes whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Routes whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Routes whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Routes extends Model
 {
     use HasFactory;
---
+++ api/app/Providers/AdminSectionsServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Routes;
+use App\Models\User;
+use SleepingOwl\Admin\Providers\AdminSectionsServiceProvider as ServiceProvider;
+
+class AdminSectionsServiceProvider extends ServiceProvider
+{
+
+    /**
+     * @var array
+     */
+    protected $sections = [
+        User::class => 'App\Http\Sections\Users',
+        Routes::class => 'App\Http\Sections\Routes',
+    ];
+
+    /**
+     * Register sections.
+     *
+     * @param \SleepingOwl\Admin\Admin $admin
+     * @return void
+     */
+    public function boot(\SleepingOwl\Admin\Admin $admin)
+    {
+    	//
+
+        parent::boot($admin);
+    }
+}
